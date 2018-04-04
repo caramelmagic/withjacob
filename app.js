d1ab4fe31795f2e23947e63e825f0b4266a92777
Kayo <kayo@kayo-yoshida-no-macbook.local>
2018-04-04 21:04:42 +0900

    I think it looks a bit better now

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 /**************************
  * Private function
  * arrayからnum個の要素をランダムに重複無しに抜き出して新しい配列を作る
+ * Randomly choose num elements of arr without duplicates
+ * and create a new array r out of them
  **************************/
-function random(array, num) {
-    var a = array;
+function random(arr, num) {
+    var a = arr;
     var t = [];
     var r = [];
     var l = a.length;
@@ -18,8 +20,18 @@ function random(array, num) {
     return r;
 }
 
+
 /**************************
- *
+ * Constructor
+ **************************/
+function Card(_cardId, _faceImgSrc, _isFaceDown){
+    this.cardId = _cardId;
+    this.faceImgSrc = _faceImgSrc;
+    this.isFaceDown = _isFaceDown;
+}
+
+/**************************
+ * Object gamePairs
  **************************/
 var gamePairs = {
     numRow : 2,
@@ -29,16 +41,14 @@ var gamePairs = {
         "img/mocha1.jpg",
         "img/mocha2.jpg"
     ],
-    faceImgSrc : [],    // numRow*numCol face images rearranged randomly
-    rowElmt : [],
-    indices : []
+    tableElmt : null,
+    rowElmts : [],
+    cellElmts : [],
+    cards : []  // array of Card objects
 }; // end var gamePairs
 
-var desk;
-// var cardImg = [];
-var cardImgElmt;
-var cellElmt;
 
+/* Once all the HTML has been loaded */
 window.onload = function() {
 
     const moment = require('moment')
@@ -52,74 +62,70 @@ window.onload = function() {
     //**** Obtain the number of rows and columns the user has selected ******
     // To be implemented
 
-    /* Set values to gamePairs.indices so that
-       It has value [0, 0, 1, 1, 2, 2, ..., numRow*numCol/2 - 1, numRow*numCol/2 - 1]
-     */
+    // create Card elements
+    for(var i=0 ; i<gamePairs.numRow * gamePairs.numCol ; i++){
+        gamePairs.cards[i] = new Card(i, 'default image source', true);
+    }
+
+    // Give values to gamePairs.cards[*].cardId so that
+    // cardId has value [0, 0, 1, 1, 2, 2, ..., numRow*numCol/2 - 1, numRow*numCol/2 - 1]
     for(var i=0 ; i< gamePairs.numRow * gamePairs.numCol/2 ; i++){
-        gamePairs.indices[2*i] = i;
-        gamePairs.indices[2*i+1] = i;
-        //gamePairs.faceImgSrc[2*i] = gamePairs.faceOrgImgSrc[i];
-        //gamePairs.faceImgSrc[2*i+1] = gamePairs.faceOrgImgSrc[i];
+        gamePairs.cards[2*i].cardId = i;
+        gamePairs.cards[2*i+1].cardId = i;
+
     }
 
-    // Randomly rearrange the order within gamePairs.indices
-    // in order to decide which face goes to which position
-    gamePairs.indices = random( gamePairs.indices, gamePairs.indices.length); //indicesを並び替える
-    console.log("gamePairs.indices: " + gamePairs.indices);
+    // Shuffle the cards
+    gamePairs.cards = random( gamePairs.cards, gamePairs.cards.length); //shuffle cards
+
+    // Give each card a face image.
+    // Create cell elements.
     for( var i=0; i< gamePairs.numRow * gamePairs.numCol ; i++){
-        gamePairs.faceImgSrc[i] = gamePairs.faceOrgImgSrc[gamePairs.indices[i]];
+        gamePairs.cards[i].faceImgSrc = gamePairs.faceOrgImgSrc[gamePairs.cards[i].cardId]; // set face image
+        gamePairs.cellElmts[i] = document.createElement("td");  // create cell element
+        // assign each cell element a cellIndex so that the program can tell which cell is clicked
+        gamePairs.cellElmts[i].setAttribute("cellIndex", i);
     }
 
-    //gamePairs.faceImgSrc = random( gamePairs.faceImgSrc, gamePairs.faceImgSrc.length ); //faceImgSrcを並び替える
-    console.log("gamePairs.faceImgSrc: " + gamePairs.faceImgSrc );
 
+    gamePairs.tableElmt = document.getElementById("table1");
+    gamePairs.tableElmt.setAttribute("border","1");
 
-
-    desk = document.getElementById("desk");
-    desk.setAttribute("border","1");
-
+    //Insert rows and cells in the table
     for(var i=0; i<gamePairs.numRow; i++){
 
         // create and insert the i-th row element
-        gamePairs.rowElmt[i] = desk.insertRow();
-        //cardImg[i] = [];
+        gamePairs.rowElmts[i] = gamePairs.tableElmt.insertRow();
 
         // for each cell in the row i .....
         for(var j=0; j<gamePairs.numCol; j++){
-
-            cellElmt = gamePairs.rowElmt[i].insertCell(0);   // create and insert a cell element
-            cellElmt.setAttribute("cardState", "back");     // face is down
-
-            cardImgElmt = document.createElement("img");    // create an img element
+            var idx = gamePairs.numCol*i+j; // index of cell at row i, column j
+            gamePairs.rowElmts[i].appendChild(gamePairs.cellElmts[idx]); // insert a cell element
+            var cardImgElmt = document.createElement("img");    // create an img element
             cardImgElmt.setAttribute("src", gamePairs.backImgSrc);    // "img/tree.jpg"
             cardImgElmt.setAttribute("width","100");
-            cellElmt.appendChild(cardImgElmt);  // insert the img element as a child of cell element
-
-
-            // there's something wrong here
-            // set card face
-            cellElmt.setAttribute("cardFaceSrc", gamePairs.faceImgSrc[gamePairs.numCol*i+j]);
-            console.log("Row:"+ i +", Col:" + j + " " + cellElmt.getAttribute("cardFaceSrc"));
-
-
-
-
-
-            // If this cell is clicked, flip the card
-            cellElmt.addEventListener('click', function(){
-                console.log(this.childNodes[0]);
-                if(this.getAttribute("cardState")=="back"){
-                    this.childNodes[0].setAttribute("src",this.getAttribute("cardFaceSrc"));
-                    this.setAttribute("cardState","face");
-                }else{
-                    this.childNodes[0].setAttribute("src","img/tree.jpg");
-                    this.setAttribute("cardState","back");
-
-                }
-
-            }); // end cellElmt.addEventListener
+            gamePairs.cellElmts[idx].appendChild(cardImgElmt);  // insert the img element as a child of cell element
 
         }  // end for
     } // end for
 
+    // If a cell is clicked, flip the card
+    for(var i=0; i<gamePairs.numRow*gamePairs.numCol; i++){
+        gamePairs.cellElmts[i].addEventListener('click', function(){
+            // Cannot replace this variable by i because  a cell is clicked
+            // only after the for loop is over, at which time i is always numRow * numCol
+            var nth = parseInt(this.getAttribute("cellIndex"));
+            console.log("nth = "+ nth);
+            console.log(this.childNodes[0]);
+            if( gamePairs.cards[nth].isFaceDown ){
+                this.childNodes[0].setAttribute("src", gamePairs.cards[nth].faceImgSrc);
+                gamePairs.cards[nth].isFaceDown = false;
+
+            }else{
+                this.childNodes[0].setAttribute("src", gamePairs.backImgSrc);
+                gamePairs.cards[nth].isFaceDown = true;
+            }
+
+        }); // end cellElmts[i].addEventListener
+    } //end for
 } // end window.onload
